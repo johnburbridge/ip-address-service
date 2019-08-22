6f9f883a473ee8f7c281ef1ea1f64f8c597d8254
John Burbridge <johnb@stitchlabs.com>
2019-08-22 00:19:56 -0700

    Containerized it

--- README.md
+++ README.md
@@ -2,3 +2,23 @@ IP Address Service
 ------------------
 A simple service that returns the server's ip address.
 
+Building:
+`./gradlew bootJar docker`
+
+Running from Gradle:
+`./gradlew bootRun`
+
+Or from docker:
+`docker run -t gcr.io/spring-sandbox-219017/ip-address-server -p 8080:8080`
+
+You shold be able to hit the following endpoints:
+```
+curl 172.17.0.2:8080
+curl 172.17.0.2:8080/interfaces
+ 
+curl 172.17.0.2:8080/actuator
+curl 172.17.0.2:8080/actuator/info
+curl 172.17.0.2:8080/actuator/health
+curl 172.17.0.2:8080/actuator/metrics
+curl 172.17.0.2:8080/actuator/metrics/system.load.average.1m
+```
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,10 +3,12 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "2.1.7.RELEASE"
 	id("io.spring.dependency-management") version "1.0.8.RELEASE"
+	id("com.palantir.docker") version "0.22.1"
 	kotlin("jvm") version "1.2.71"
 	kotlin("plugin.spring") version "1.2.71"
 }
 
+val appName = "ip-address-server"
 group = "org.burbridge"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_1_8
@@ -28,9 +30,34 @@ springBoot {
 	buildInfo()
 }
 
-tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "1.8"
+tasks {
+	withType<KotlinCompile> {
+		kotlinOptions {
+			freeCompilerArgs = listOf("-Xjsr305=strict")
+			jvmTarget = "1.8"
+		}
+	}
+
+	docker {
+		val build = build.get()
+		val bootJar = bootJar.get()
+		val dockerImageName = "gcr.io/spring-sandbox-219017/$appName"
+
+		dependsOn(build)
+
+		name = "$dockerImageName:latest"
+		tag("current", "$dockerImageName:$version")
+		tag("latest", "$dockerImageName:latest")
+		files(bootJar.archiveFile)
+		setDockerfile(file("$projectDir/src/main/docker/Dockerfile"))
+		buildArgs(
+				mapOf(
+						"JAR_FILE" to bootJar.archiveFileName.get(),
+						"JAVA_OPTS" to "-XX:-TieredCompilation",
+						"PORT" to "8080"
+				)
+		)
+		pull(true)
 	}
 }
+
